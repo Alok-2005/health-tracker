2359b693975b61b2ab3a28849a13fa4c9ae8eac6
Alok-2005 <alokchaturvedi190@gmail.com>
2025-01-31 15:33:15 +0530

    done

--- health-tracker-final/dist/health-tracker/server/angular-app-manifest.mjs
+++ health-tracker-final/dist/health-tracker/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 24720, hash: '3dfc5c073b9c9452042a94a542e8f4db9339ee1be21605d7bdf7a26cbdf68a72', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1032, hash: '894640e754f7ab6a8060ddce349525c3a9008fee62178b32fb8e025807d00e2d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 144228, hash: '07baa2441743b8553c4b3c7f972ae920f20adf7d3a5d24b1cb50ee5c8927a797', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'styles-ZBNXGDKG.css': {size: 33343, hash: 'SOzW9cw5QlY', text: () => import('./assets-chunks/styles-ZBNXGDKG_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 24748, hash: '1e1a026e806f6c9b245612375c3d2df1822f1a7e213ee6113fdf5fa1c385141e', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1032, hash: 'bb253ee9cea9878e27acd2d4d177b9f1689361eb5b146994cbda390ced357ed0', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 160477, hash: '2b320df006d46bf379c6ffa39cf50d621701ea5093645474b658ca79a36e33a8', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'styles-XAFF36S5.css': {size: 34412, hash: 'tkFMq2GOzfQ', text: () => import('./assets-chunks/styles-XAFF36S5_css.mjs').then(m => m.default)}
   },
 };
